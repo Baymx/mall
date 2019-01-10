37de2ce318cd3075f08c7e3c56dd6aa99761a0a5
qianweiwei <qianweiwei@pingchang666.com>
2019-01-10 18:49:12 +0800

    提交

--- src/pages/Home/components/RotationSlider/RotationSlider.jsx
+++ src/pages/Home/components/RotationSlider/RotationSlider.jsx
@@ -1,9 +1,8 @@
 /* eslint no-mixed-operators: 0 */
-import React, { Component } from 'react';
-import { Slider } from '@icedesign/base';
+import React, { Component } from "react";
+import { Slider } from "@icedesign/base";
 
 export default class Rotation extends Component {
-
   constructor(props) {
     super(props);
     this.state = {};
@@ -12,29 +11,40 @@ export default class Rotation extends Component {
   render() {
     const bigSlides = [
       {
-        url: "https://fms.res.meizu.com/dms/2018/12/29/d05f4885-c98d-41c9-9251-ee213fc15e68.jpg",
-        text: "磁带播放器换肤设计大赛"
+        url:
+          "https://fms.res.meizu.com/dms/2019/01/08/efc114da-2afc-4e1d-bdff-56bcf1213340.jpg",
       },
       {
-        // background-image: -webkit-image-set(url("https://fms.res.meizu.com/dms/2018/12/29/067699fb-28be-4cce-b95e-8509fbdc473c.jpg") 1x, url("https://fms.res.meizu.com/dms/2018/12/29/d05f4885-c98d-41c9-9251-ee213fc15e68.jpg") 2x); width: 1905px; height: 670px;
-        url: "https://openfile.meizu.com/group1/M00/06/F3/Cgbj0VwbayCADvymAAhhybLeHtc355.jpg",
-        text: "手机淘宝皮肤征集"
+        url:
+          "https://fms.res.meizu.com/dms/2018/12/29/d05f4885-c98d-41c9-9251-ee213fc15e68.jpg",
       },
       {
-        url: "https://img.alicdn.com/tps/TB1ikP.NVXXXXaYXpXXXXXXXXXX-1000-300.jpg",
-        text: "设计赋能公益"
+        url:
+          "https://openfile.meizu.com/group1/M00/06/F3/Cgbj0VwbayCADvymAAhhybLeHtc355.jpg",
       },
       {
-        url: "https://img.alicdn.com/tps/TB1s1_JNVXXXXbhaXXXXXXXXXXX-1000-300.jpg",
-        text: "淘公仔设计大赛"
+        url:
+          "https://openfile.meizu.com/group1/M00/06/F1/Cgbj0VwZ-AuAGG2iAAbaHUcx_Hk209.jpg",
+      },
+      {
+        url:
+          "https://fms.res.meizu.com/dms/2018/12/29/38df385d-0f54-44d1-a0c1-485d360e8d56.jpg",
       }
     ];
     return (
       <div>
-        <Slider arrowSize="large" autoplay={true} lazyLoad={true} arrows={false} autoplaySpeed={30000}>
+        <Slider
+          arrowSize="large"
+          autoplay={true}
+          arrows={false}
+        >
           {bigSlides.map((item, index) => (
-            <div key={index} className="slider-img-wrapper">
-              <img src={item.url} alt={item.text} />
+            <div
+              key={index}
+              className="slider-img-wrapper"
+              style={styles.slider}
+            >
+              <img src={item.url} />
             </div>
           ))}
         </Slider>
@@ -42,3 +52,12 @@ export default class Rotation extends Component {
     );
   }
 }
+const styles = {
+  homepage: {
+    background: "#fff",
+    overflow: "hidden"
+  },
+  slider: {
+    width: document.body.clientWidth + "px"
+  }
+};
