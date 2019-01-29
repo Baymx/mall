c6e24d9be9a82c24ba301677447070cf49c2284d
qianweiwei <qianweiwei@pingchang666.com>
2019-01-29 18:23:37 +0800

    修改发布配置

--- src/components/Recommend/components/SlidesShow/SlidesShow.jsx
+++ src/components/Recommend/components/SlidesShow/SlidesShow.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Slider } from '@icedesign/base';
-import Img from '@icedesign/img';
 export default class SlidesShow extends Component {
   slides = () => {
     const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
